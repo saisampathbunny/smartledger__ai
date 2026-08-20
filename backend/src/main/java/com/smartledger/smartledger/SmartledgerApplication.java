aa0e52d85516b2d65572376cd28bbcd7c809e21d
saisampathbunny <149319198+saisampathbunny@users.noreply.github.com>
2026-08-20 17:26:58 +0530

    Create SmartledgerApplication.java

---
+++ backend/src/main/java/com/smartledger/smartledger/SmartledgerApplication.java
@@ -0,0 +1,16 @@
+package com.smartledger.smartledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartledgerApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(
+                SmartledgerApplication.class,
+                args
+        );
+    }
+}
